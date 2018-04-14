212d4726ca1956c75a658b6a3bab693bb701377a
Erick de Moura <erick@cartesi.io>
2018-04-14 17:27:47 +0800

    Adjustments for local tests of gitlab-ci

--- src/emulator/tests.sh
+++ src/emulator/tests.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-for t in tests/*.bin; do echo $t; lua run.lua --batch --boot-image=$t || echo FAIL; done
+root_dir="$( cd "$(dirname "$0")" ; pwd -P )"
+cd $root_dir
+lua run.lua --batch --boot-image=tests/rv64um-v-divw.bin
+#for t in tests/*.bin; do echo $t; lua run.lua --batch --boot-image=$t || echo FAIL; done

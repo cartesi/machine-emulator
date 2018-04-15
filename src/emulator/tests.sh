c8d4d6dec9e147ffc508eb2870245f4c1fe664c8
Erick de Moura <erick@cartesi.io>
2018-04-15 23:52:42 +0800

    CI docker runner locally functional

--- src/emulator/tests.sh
+++ src/emulator/tests.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 root_dir="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $root_dir
-lua run.lua --batch --boot-image=tests/rv64um-v-divw.bin
-#for t in tests/*.bin; do echo $t; lua run.lua --batch --boot-image=$t || echo FAIL; done
+for t in tests/*.bin; do echo $t; lua run.lua --batch --boot-image=$t || echo FAIL; done

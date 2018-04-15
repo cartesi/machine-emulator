82dab60c7993af31fbcc019b9497b2409d230f94
Erick de Moura <erick@cartesi.io>
2018-04-16 01:49:23 +0800

    Added return status  to test bash script

--- src/emulator/tests.sh
+++ src/emulator/tests.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 root_dir="$( cd "$(dirname "$0")" ; pwd -P )"
+errors_count=0
+
 cd $root_dir
-for t in tests/*.bin; do echo $t; lua run.lua --batch --boot-image=$t || echo FAIL; done
+
+for t in tests/*.bin; do echo $t; lua run.lua --batch --boot-image=$t || ((errors_count++)); done
+echo $errors_count  
+
+exit $?

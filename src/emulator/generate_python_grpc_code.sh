a8ace673759e4b4b404ee3ff7e58ef5f1753cfb8
Carlo Fragni <carlo@cartesi.io>
2019-05-03 18:44:01 -0300

    Move python gprc script to cartesi-grpc

--- src/emulator/generate_python_grpc_code.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-GRPC_DIR=../../cartesi-grpc
-GRPC_PY_DIR=$GRPC_DIR/py
-python -m grpc_tools.protoc -I$GRPC_DIR --python_out=$GRPC_PY_DIR --grpc_python_out=$GRPC_PY_DIR $GRPC_DIR/core.proto
-python -m grpc_tools.protoc -I$GRPC_DIR --python_out=$GRPC_PY_DIR $GRPC_DIR/cartesi-base.proto
-python -m grpc_tools.protoc -I$GRPC_DIR --python_out=$GRPC_PY_DIR --grpc_python_out=$GRPC_PY_DIR $GRPC_DIR/manager-low.proto
-
-

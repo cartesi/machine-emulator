eac835f61f84a59315056369cba94ff04b1a56fd
Victor Fusco <victor@cartesi.io>
2019-07-23 11:42:41 -0300

    Remove ROM source code
    The ROM source has been moved to it's own repository

--- src/rom/src/rom-defines.h
+++
@@ -1,7 +0,0 @@
-#ifndef ROM_DEFINES_H
-#define ROM_DEFINES_H
-
-#define CLOCK_FREQ             1000000000 ///< 1 GHz frequency is arbitrary
-#define DEVICE_TREE_MAX_SIZE   0x10000 ///< Device tree buffer size
-
-#endif /* end of include guard: ROM_DEFINES_H */

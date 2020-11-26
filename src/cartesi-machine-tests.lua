c77346b635d1664bf7ef8752dbe21f8677c9ac11
Victor Fusco <victor@cartesi.io>
2020-11-26 16:13:16 -0300

    Add new version check test

--- src/cartesi-machine-tests.lua
+++ src/cartesi-machine-tests.lua
@@ -208,6 +208,7 @@ local tests = {
   {"xpie_exceptions.bin", 51},
   {"htif_devices.bin", 498, 42, {10, 20, 30, 45, 55, 65}},
   {"dont_write_x0.bin", 44},
+  {"version_check.bin", 30},
 }
 
 -- Print help and exit

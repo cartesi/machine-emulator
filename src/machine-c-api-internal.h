6d9de5836e84c49961f73c82f22d543e19acf774
Eduardo Bart <edub4rt@gmail.com>
2023-11-10 11:43:59 -0300

    refactor: don't expose internal symbols in the public C API

--- src/machine-c-api-internal.h
+++ src/machine-c-api-internal.h
@@ -25,42 +25,42 @@
 #include "semantic-version.h"
 
 /// \brief Helper function that returns error result from C api function
-CM_API int cm_result_failure(char **err_msg);
+int cm_result_failure(char **err_msg);
 
 /// \brief Helper function that returns success result from C api function
-CM_API int cm_result_success(char **err_msg);
+int cm_result_success(char **err_msg);
 
 /// \brief Helper function that create empty string in case
 /// that C string is NULL
-CM_API std::string null_to_empty(const char *s);
+std::string null_to_empty(const char *s);
 
 /// \brief Helper function that parses machine configuration cartesi::machine_config
 /// from C api structure cm_machine_config
-CM_API cartesi::machine_config convert_from_c(const cm_machine_config *c_config);
+cartesi::machine_config convert_from_c(const cm_machine_config *c_config);
 
 /// \brief Helper function that parses machine runtime configuration
 /// from C api structure cm_machine_runtime_config
-CM_API cartesi::machine_runtime_config convert_from_c(const cm_machine_runtime_config *c_config);
+cartesi::machine_runtime_config convert_from_c(const cm_machine_runtime_config *c_config);
 
 /// \brief Helper function converts machine configuration to C api structure
-CM_API cm_machine_config *convert_to_c(const cartesi::machine_config &cpp_config);
+cm_machine_config *convert_to_c(const cartesi::machine_config &cpp_config);
 
 /// \brief Helper function converts a semantic version to C api structure
-CM_API cm_semantic_version *convert_to_c(const cartesi::semantic_version &cpp_version);
+cm_semantic_version *convert_to_c(const cartesi::semantic_version &cpp_version);
 
 /// \brief Helper function that parses hash from C api structure
-CM_API cartesi::machine_merkle_tree::hash_type convert_from_c(const cm_hash *c_hash);
+cartesi::machine_merkle_tree::hash_type convert_from_c(const cm_hash *c_hash);
 
 /// \brief Helper function that parses access log tyoe from C
-CM_API cartesi::access_log::type convert_from_c(const cm_access_log_type *type);
+cartesi::access_log::type convert_from_c(const cm_access_log_type *type);
 
 /// \brief Helper function that parses access log from C api structure
-CM_API cm_access_log *convert_to_c(const cartesi::access_log &cpp_access_log);
+cm_access_log *convert_to_c(const cartesi::access_log &cpp_access_log);
 
 /// \brief Helper function converts access log to C api structure
-CM_API cartesi::access_log convert_from_c(const cm_access_log *c_acc_log);
+cartesi::access_log convert_from_c(const cm_access_log *c_acc_log);
 
 /// \brief Helper function converts C++ string to allocated C string
-CM_API char *convert_to_c(const std::string &cpp_str);
+char *convert_to_c(const std::string &cpp_str);
 
 #endif // CM_C_API_INTERNAL_H

66c56661829f6ee0c28d0dbd3785c5b8a92d0ba2
Marcos Pernambuco Motta <1091485+mpernambuco@users.noreply.github>
2026-09-06 17:16:49 -0300

    refactor: type cm_set_temp_data generically

--- src/cm-internal.hpp
+++ src/cm-internal.hpp
@@ -22,7 +22,6 @@
 #include <vector>
 
 #include "cm.h"
-#include "step-log-data.hpp"
 
 /// \brief Helper function that returns error result from C api function
 CM_API cm_error cm_result_failure();
@@ -34,6 +33,6 @@ CM_API cm_error cm_result_success();
 CM_API const char *cm_set_temp_string(const std::string &s);
 
 /// \brief Helper function that stores binary data to a temporary thread local.
-CM_API const uint8_t *cm_set_temp_data(cartesi::step_log_data data);
+CM_API const uint8_t *cm_set_temp_data(std::vector<unsigned char> data);
 
 #endif // CM_INTERNAL_HPP
